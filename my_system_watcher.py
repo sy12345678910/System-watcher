264b7a4c1be767df1ec0c62c0b1e11a510d120f9
Sasha Yuvko <sasha.yuvko@gmail.com>
2026-06-12 10:45:08 +0300

    1.0

--- my_system_watcher.py
+++ my_system_watcher.py
@@ -1,26 +1,30 @@
 import psutil
-from PySide6.QtWidgets import QSystemTrayIcon, QMenu, QApplication, QWidget
-from PySide6.QtCore import QObject, QTimer
+from PySide6.QtWidgets import QSystemTrayIcon, QMenu, QApplication, QStyle
+from PySide6.QtCore import QObject, QTimer, Slot
+from PySide6.QtGui import QPixmap
 
 
-class SystemWatcher(QWidget):
+class SystemWatcher(QObject):
     def __init__(self):
         super().__init__()
         self.tray_icon = QSystemTrayIcon(self)
+        std_icon = QPixmap('cpu.png')  
+        self.tray_icon.setIcon(std_icon)
+
         self.setup_menu()
-        self.setup_timer(self.update_status)
-        
-    def setup_menu(self):
-        self.menu = QMenu(self)
-        self.exit_action = self.menu.addAction('exit')
-        self.exit_action.triggered.connect(QApplication.quit)
-        self.tray_icon.setContextMenu(self.menu)
-        
-    def setup_timer(self, func):
         self.timer = QTimer(self)
-        self.timer.timeout.connect(func)
+        self.timer.timeout.connect(self.update_status)
         self.timer.start(1000)
         
+        self.tray_icon.show()
+        
+    def setup_menu(self):
+        self.menu = QMenu()
+        exit_action = self.menu.addAction('exit')
+        exit_action.triggered.connect(QApplication.quit)
+        self.tray_icon.setContextMenu(self.menu)
+    
+    @Slot()
     def update_status(self):
         cpu_usage = psutil.cpu_percent()
         ram_usage = psutil.virtual_memory().percent
